cc3a852b28a493a1144b87eb1b9244b3163dbf5e
Irving Popovetsky <irving@chef.io>
2019-03-08 12:03:09 -0800

    Version bump for impending release, also fix install of awscli on RHEL again
    Signed-off-by: Irving Popovetsky <irving@chef.io>

--- files/before.sh
+++ files/before.sh
@@ -3,11 +3,12 @@
 # Below are needed for RHEL, which lacks niceties like the AWS tools and lvm
 if [[ ! -f /bin/aws ]]; then
   echo ">>> Installing awscli package"
-  yum install -y python-cryptography python-docutils
-  # Thanks Redhat for hiding these in add-on repos, gotta get these from centos
-  rpm -ivh \
-    http://mirror.centos.org/centos/7/os/x86_64/Packages/python-s3transfer-0.1.10-8.el7.noarch.rpm \
-    http://mirror.centos.org/centos/7/updates/x86_64/Packages/awscli-1.14.28-5.el7_5.1.noarch.rpm
+  yum install -y unzip
+  yum erase -y awscli python2-botocore python-s3transfer
+  curl -sOL https://s3.amazonaws.com/aws-cli/awscli-bundle.zip
+  unzip awscli-bundle.zip
+  ./awscli-bundle/install -i /usr/local/aws -b /bin/aws
+  rm -rf awscli-bundle awscli-bundle.zip
 fi
 
 if [[ ! -f /opt/aws/bin/cfn-init ]]; then
